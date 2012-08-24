e407da147021e965b0b84ec38a506195185592f8
Sebastian Geiger <sbastig@gmx.net>
2012-08-24 17:40:32 +0200

    CalcStack: add printResult() function

--- src/at/ac/prog/calculator/engine/CalcStack.java
+++ src/at/ac/prog/calculator/engine/CalcStack.java
@@ -6,4 +6,10 @@ public class CalcStack extends Stack<Object> {
 
 	private static final long serialVersionUID = -2121393778527282706L;
 
+	public void printResult() {
+		if(this.size() > 0) {
+			System.out.println("Calculation Result: " + this.pop());
+		}
+	}
+
 }
