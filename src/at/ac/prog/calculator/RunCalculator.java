b6e33ca8f6335cb32a5ad1d5aa45c131a384723c
spookyTU <ralph.hoch@tuwien.ac.at>
2012-09-25 01:54:44 +0200

    start class of programmable calculator

--- src/at/ac/prog/calculator/RunCalculator.java
+++ src/at/ac/prog/calculator/RunCalculator.java
@@ -1,11 +1,19 @@
 package at.ac.prog.calculator;
 
+import at.ac.prog.calculator.engine.CalcExecutor;
 import at.ac.prog.calculator.ui.CalculatorUI;
 
 public class RunCalculator {
 	
 	public static void main(String[] args) {
 		CalculatorUI calcUI = new CalculatorUI();
+		
+		CalcExecutor executor = new CalcExecutor();
+		executor.registerListener(calcUI);
+		
+		calcUI.init(executor);
+		
+		
 	}
 
 }
